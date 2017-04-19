af72fefe3447ea632d8c4d999f78a8343b679508
OSI-Writer <bbelisle@osisoft.com>
2017-04-19 10:01:35 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,9 @@ import os
 
 # -- General configuration ------------------------------------------------
 
+# uncommented following line and changed to v1.4
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
